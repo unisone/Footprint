ba1fe30d0af36e6579a6311887bae92ccc8f6f91
Alex Zay <alexvzay@gmail.com>
2018-09-14 03:10:00 +0000

    update dbURL

--- app.js
+++ app.js
@@ -15,9 +15,13 @@ var express     = require("express"),
 var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
+ 
+console.log(process.env.DATABASEURL);
     
-// mongoose.connect("mongodb://localhost/yelp_camp_v3");
-mongoose.connect("mongodb://alex:pass1907@ds155252.mlab.com:55252/footprint");
+mongoose.connect("process.env.DATABASEURL");
+// mongoose.connect("mongodb://alex:pass1907@ds155252.mlab.com:55252/footprint");
+
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
